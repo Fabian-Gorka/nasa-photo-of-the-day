665552b9fc87483623e5b0ec7b771e9bdd1ed914
Fabian-Gorka <fabian-gorka@lambdastudents.com>
2021-02-19 01:18:29 -0600

    second part done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
 import SpaceStuff from './components/SpaceStuff.js';
+import styled from 'styled-components'
 
 function App() {
 
@@ -14,26 +15,31 @@ function App() {
         setInformation(res.data.explanation);
         setTitle(res.data.title);
         setDate(res.data.date)
-        setImage(res.data.url)
+        setImage(res.data.hdurl)
         setVideo(res.data.media_type === 'video' ? res.data.url : undefined)
     }).catch(err => {
     console.log(err)
     })
   } 
-  useEffect(getData,[]) 
+  useEffect(getData,[])
+  
     const [information, setInformation] = useState('')
     const [title, setTitle] = useState('')
     const [image, setImage] = useState('')
     const [date, setDate] = useState('')
     const [video, setVideo] = useState('')
+
+    const Styapp = styled.div`
+  background-color:whitesmoke;
+  `
   return (
-    <div className="App">
+    <Styapp className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
       <SpaceStuff information = {information} title = {title} date = {date} image = {image} video = {video}/>
-    </div>
+    </Styapp>
   );
 }
 
